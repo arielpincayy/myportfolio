446a1ab759ac7f5d2eed73ecfe1f4b3544853a49
arielpincayy <arielpincay812@gmail.com>
2025-07-07 12:10:19 -0500

    add:Curriculum.pdf

--- app/page.tsx
+++ app/page.tsx
@@ -30,8 +30,8 @@ export default function Portfolio() {
             </Link>
           </nav>
             <a
-            href="/pepe.pdf"
-            download="pepe.pdf"
+            href="/Curriculum.pdf"
+            download="Curriculum.pdf"
             className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2"
             >
             Download CV
